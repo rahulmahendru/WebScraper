2719eb2ba13e16b9d44535b4130aff5268b3fbfb
rahulmahendru <rkm687304@gmail.com>
2020-09-18 22:57:50 +0530

    Basic WebScraper implemented

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,2 +1,8 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
+
+
+URL = input("Enter a URL: ")
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup.prettify())
