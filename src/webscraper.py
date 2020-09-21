0832f2fa597daa38491d1a7b0e356b24cbbde4d6
rahulmahendru <rkm687304@gmail.com>
2020-09-21 12:32:14 +0530

    HTML and XML scraping implemented

--- src/webscraper.py
+++ src/webscraper.py
@@ -1,8 +1,19 @@
 import requests 
 from bs4 import BeautifulSoup
 
+def getHTML(page):
+    soup = BeautifulSoup(page.content, 'html.parser')
+    file = open("HTML_file.txt", "x")
+    file.write(soup.prettify())
+    file.close()
+
+def getXML(page):
+    soup = BeautifulSoup(page.content, 'lxml')
+    file = open("XML_file.txt", "x")
+    file.write(soup.prettify())
+    file.close()
 
 URL = input("Enter a URL: ")
 page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+getHTML(page)
+getXML(page)
